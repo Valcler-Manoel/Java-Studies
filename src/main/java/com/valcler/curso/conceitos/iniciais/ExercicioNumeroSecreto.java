7e9ac01460ed337a9a213acad55201e3ac82226c
Valcler-Manoel <gavin7297@gmail.com>
2025-05-20 21:59:34 -0300

    feat: Configura projeto Maven inicial e .gitignore
    Cria a estrutura de projeto Maven para os exercícios do curso da Udemy.\nConfigura o arquivo .gitignore para ignorar arquivos de IDE e de build (target/).\nIsso garante uma organização profissional para o código-fonte.

---
+++ src/main/java/com/valcler/curso/conceitos/iniciais/ExercicioNumeroSecreto.java
@@ -0,0 +1,26 @@
+package com.valcler.curso.conceitos.iniciais;
+
+import java.util.Scanner;
+
+public class ExercicioNumeroSecreto {
+
+	public static void main(String[] args) {
+		int secretNumber = 7;
+		int x = 0;
+		Scanner scanner = new Scanner(System.in);
+
+		while (x != secretNumber) {
+			System.out.print("Guess a number between 1 and 10: ");
+			x = scanner.nextInt();
+
+			if (x != secretNumber) {
+				System.out.println("Wrong!");
+			} else {
+				System.out.println("Congratulations, you're right!");
+			}
+		}
+
+		scanner.close();
+	}
+
+}
---
+++ src/main/java/com/valcler/curso/conceitos/iniciais/ExercicioTemperatura.java
@@ -0,0 +1,14 @@
+package com.valcler.curso.conceitos.iniciais;
+
+public class ExercicioTemperatura {
+
+	public static void main(String[] args) {
+
+		int temperatura = 25;
+		if (temperatura > 30) {
+			System.out.println("Está muito quente!");
+		} else {
+			System.out.println("A temperatura está agradável.");
+		}
+	}
+}
